d5cfd2a37cc07350c830120f91629229efa3ab10
Trần Đăng Khoa <tdkhoa2409@gmail.com>
2025-05-23 22:46:47 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import AppRouter from '@routers/app-router';
 
 // ----------------------------------------------------------------------
@@ -5,7 +6,9 @@ import AppRouter from '@routers/app-router';
 export default function App() {
   return (
     <>
-      <AppRouter />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
+        <AppRouter />
+      </GoogleOAuthProvider>
     </>
   );
 }
